77542c795b84d3b170ad550d36c4204c8ea95a18
Achj03 <0133437@kdu-online.com>
2023-07-08 14:36:48 +0800

    update gun

--- Assets/gunmechanic.cs
+++ Assets/gunmechanic.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class gunmechanic : MonoBehaviour
 {
+    private float xspeed;
+    private float lastPosition;
+    private bool rotate;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,31 @@ public class gunmechanic : MonoBehaviour
         Vector2 direction = mousePosition - transform.parent.position;
         float angle = Vector2.SignedAngle(Vector2.right, direction);
         Vector3 targetRotation = new Vector3(0, 0, angle);
+        /*xspeed = (transform.position.x - lastPosition) / Time.deltaTime;
+        lastPosition = transform.position.x;
+        if(xspeed<0){
+            if(rotate){
+                transform.position = new Vector3(-0.75f,0,0) + transform.parent.position;
+                rotate = false;
+            }
+            else{
+                if(xspeed<3){
+                    rotate = true;
+                }
+            }
+        }
+        else
+        {
+            if(rotate){
+                transform.position = new Vector3(0.75f,0,0) + transform.parent.position;
+                rotate = false;
+            }
+            else{
+                if(xspeed>3){
+                    rotate = true;
+                }
+            }
+        }*/
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), 20);
     }
 }
